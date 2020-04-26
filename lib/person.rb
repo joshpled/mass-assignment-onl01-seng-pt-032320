d333e98407d451c2a5c6b318b55f0584679a8c8f
Joshua Perez Leduc <joshuapleduc@gmail.com>
2020-04-26 05:08:44 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -4,5 +4,6 @@ attr_accessor :name, :birthday, :hair_color, :eye_color, :height,
 :wrist_size, :glove_size, :pant_length, :pant_width
 
   def initialize({})
+    {}.each {|key, value| self.send(("#{key}="), value)}
   end
 end
